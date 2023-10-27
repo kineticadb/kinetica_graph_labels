da4aa19135ceca1326611b4b23d8d503ea331efc
kkaramete <kkaramete@kinetica.com>
2023-10-27 17:52:37 -0400

    Added two new tests and some rudimentary testing framework.

--- TestLabels.cpp
+++ TestLabels.cpp
@@ -4,6 +4,40 @@
 #include <string>
 #include "GraphLabelContainer.h"
 
+int test_mimic_graph(std::ostream &out)
+{
+    // 1. Assume Dict encoded indexes for unique label strings
+    std::map<std::string,std::size_t> vlabels =
+    { {"self",1}, {"ceo",2}, {"principal",3}, {"expert",4}, {"master",5}, {"vp",6} };
+
+    // 2. Assume the node names as dict encoded string to graph node indexes
+    std::map<std::string,std::size_t> nodes = 
+    { {"kaan",1}, {"eli", 2}, {"shouvik", 3}, {"nima",4}, {"steve",5}, {"john",6} };
+
+    // 3. Associate node with the label
+    GraphLabelContainer glc;
+
+    glc.addLabel(nodes["shouvik"],vlabels["expert"]);
+    glc.addLabel(nodes["shouvik"],vlabels["master"]);
+    glc.addLabel(nodes["kaan"],vlabels["expert"]);
+    glc.addLabel(nodes["kaan"],vlabels["self"]);
+    glc.addLabel(nodes["eli"],vlabels["vp"]);
+    glc.addLabel(nodes["eli"],vlabels["expert"]);
+    glc.addLabel(nodes["nima"],vlabels["ceo"]);
+    glc.addLabel(nodes["nima"],vlabels["expert"]);
+
+    glc.addLabel(nodes["steve"],vlabels["principal"]);
+
+    glc.addLabel(nodes["john"],vlabels["expert"]);
+    glc.addLabel(nodes["john"],vlabels["vp"]);
+    
+
+    glc.print(out);
+
+    return 1;
+}
+
+
 int test_A241_label_container(std::ostream &out)
 {
     GraphLabelContainer c;
@@ -99,8 +133,99 @@ int test_A241_label_container(std::ostream &out)
     return 1;
 }
 
+typedef std::map<std::string, int (*)(std::ostream&)> TestMapType;
+TestMapType tmap;
+
+#define REGISTER(T) \
+    tmap[#T] = &T;
 
 int main(int c, char** v)
 {
-   return test_A241_label_container(std::cout);
+	
+   REGISTER(test_A241_label_container)
+   REGISTER(test_mimic_graph)
+
+   std::ostream *m_out = &std::cout;
+   if(c == 1)
+   {
+       std::cout <<"########################################################" << std::endl;
+       std::cout << "TESTS for labelling containers" << std::endl;
+       std::cout << "Usage: TestLabels.x <option>" << std::endl;
+       std::cout << "Options: " << std::endl;
+       std::cout << "     -l : lists all available tests..." << std::endl;         
+       std::cout << "     -q : Runs all tests in quiet mode..." << std::endl;         
+       std::cout << "     -t [0..n] : tests the nth test..." << std::endl;         
+       std::cout << "     -v : Runs all tests in verbose mode..." << std::endl;
+       std::cout <<"########################################################" << std::endl;
+       return 1;
+   }
+   int tnum = 0;
+   bool list = false;
+   for (int i = 1; i < c; ++i)
+   {
+	   std::string st = v[i];
+	   if(st == "-v")
+	   {
+	      std::cout << "running in verbose mode..." << std::endl;
+	   }
+	   if(st == "-q")
+	   {
+              m_out = new std::ostream(NULL);
+	      std::cout << "running in quite mode..." << std::endl;
+	   }
+	   if(st == "-t")
+	   {
+	      if(c < i+1)
+	      {
+		  std::cout << "See usage.." << std::endl;
+		  return 0;
+	      }
+	      tnum = atoi(v[i+1]);
+	   }
+           if(st == "-l")
+	   {
+	      list = true;
+	   }	      
+   }
+   
+   
+   int cnt = 0;
+   int nf = 0;
+   for(auto it = tmap.begin(); it != tmap.end(); ++it)
+   {
+
+           ++cnt;
+	   if(list)
+	   {
+	      std::cout << "Test " << cnt << ":" << it->first << std::endl;
+              continue;
+	   }	      
+	   
+	   if(tnum && cnt != tnum)
+		continue;
+	   std::cout << "Testing " << cnt << ":" << it->first;
+	   int res = 1;
+	    try
+            {
+                res = (it->second)(*m_out);
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+                res = 0;
+            }
+	    if(!res)
+	    {
+		    std::cout << " failed..." << std::endl;
+		    ++nf;
+	    }
+	    else
+		    std::cout << " passed..." << std::endl;
+   }
+
+   if(!list)
+     std::cout << "Tests failed = " << nf << " out of " << cnt << "." << std::endl;
+   else
+     std::cout << "Tests available : " << cnt << "." << std::endl;  
+   return 1;
 }
